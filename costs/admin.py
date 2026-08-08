620d44cc106de6e1c0625b840c768c3f5dd514ab
Dina Khoshrou <dinakhoshru.com>
2026-08-08 13:43:23 +0330

    ORM practice and English sample data

--- costs/admin.py
+++ costs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from costs.models import Category, Expense, Wallet
+
+admin.site.register(Category)
+admin.site.register(Expense)
+admin.site.register(Wallet)
--- costs/models.py
+++ costs/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Expense(models.Model):
+    amount = models.IntegerField()
+    note = models.CharField(max_length=200)
+    date = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.amount} - {self.note} - {self.date}"
+
+class Wallet(models.Model):
+    wallet = models.CharField(max_length=200)
+    inventory = models.IntegerField()
