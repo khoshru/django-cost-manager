87e7d1744b9ec23b96967e71b43f7b3099f2b6b5
Dina Khoshrou <dinakhoshru.com>
2026-08-08 16:08:08 +0330

    First views and URL routing

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from costs.views import hello, total
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("hello/", hello),
+    path("total/", total ),
 ]
--- costs/views.py
+++ costs/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from costs.models import Expense
+from django.db.models import Sum
 
-# Create your views here.
+def hello(request):
+    expenses = Expense.objects.all()
+    text = ""
+    for e in expenses:
+        text += f"{e.note} - {e.amount}<br>"
+    return HttpResponse(text)
+
+def total(request):
+    result = Expense.objects.aggregate(Sum("amount"))
+    return HttpResponse(result["amount__sum"])
+
