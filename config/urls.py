2872cc53ec1b131fe1864e1518507154fee2e558
Dina Khoshrou <dinakhoshru.com>
2026-08-12 13:06:57 +0330

    Add dj-database-url and root redirect

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from costs.views import expense_list, total_api,WalletViewSet, ExpenseViewSet, CategoryViewSet
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.views.generic import RedirectView
 
 router = DefaultRouter()
 router.register("expenses", ExpenseViewSet, basename="expense")
@@ -32,5 +33,5 @@ urlpatterns = [
     path("api/", include(router.urls)),
     path("api/token/", TokenObtainPairView.as_view()),
     path("api/token/refresh/", TokenRefreshView.as_view()),
-
+    path("", RedirectView.as_view(url="/api/")),
 ]
